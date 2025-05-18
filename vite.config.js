7b6e905f08b6d8ce7b9dbcbb5464a9c333a87cde
hu3ein89 <halavi8968@gmail.com>
2025-05-18 17:15:39 +0430

    meal-store

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/meal-store/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,json}'],
+        navigateFallback: '/meal-store/index.html'
+      },
       manifest: {
         name: 'Online Store',
         short_name: 'Store',
-        start_url: '/',
+        start_url: '/meal-store/',
         display: 'standalone',
         theme_color: '#001529',
+        background_color: '#ffffff',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/meal-store/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: '/meal-store/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -29,14 +35,18 @@ export default defineConfig({
     })
   ],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom'],
-          antd: ['antd','@ant-design/icons']
-        }
+          react: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd', '@ant-design/icons'],
+          vendor: ['lodash', 'axios']
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     },
-    chunkSizeWarningLimit: 1500, // Increase warning limit to 1000kB
+    chunkSizeWarningLimit: 1600
   }
-})
+});
